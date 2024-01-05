db8815c6160877af3786c18e26a3394394e21661
abdulalkout <abdul.alkout@gmail.com>
2024-01-05 10:06:21 -0600

    Anilmals

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -3,12 +3,14 @@ import React from "react";
 //-----------------------Components
 import Nav from "../../components/nav/Nav";
 import Introduction from "../../components/introduction/Introduction";
+import AnimalsComponent from "../../components/animalsComponent/AnimalsComponent";
 
 function Home() {
   return (
     <div>
       <Nav />
       <Introduction />
+      <AnimalsComponent />
     </div>
   );
 }
