ab80c6b562437e5abe07e5723aaf2fe53306eb55
Tesean Ferguson <tferguson1028@outlook.com>
2024-01-05 14:43:05 -0500

    Fixes

--- client/src/pages/About/About.jsx
+++ client/src/pages/About/About.jsx
@@ -3,44 +3,44 @@ import Nav from "../../components/nav/Nav";
 import "./About.css";
 
 
-function About() {
+function About()
+{
   return (
     <div>
-    <Nav />
-    <div className="About">
-    <div className="Pawsome">
-    <h1> About Pawsome Prints!!!</h1>
-    </div>
-    <div className="firstpics">
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRn81BHEGTzMbsr9XNr43MyFpfRzeyh9BY9CQ&usqp=CAU" />
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqKAbyX59TuX8k1KNeC66K0NWZL3PGss8fig&usqp=CAU" />
-      </div>
-      <div className="secondpics">
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbqrw3qH4cQdfAE233ZKct5OHCgfATey9bLw&usqp=CAU" />
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0EvPgBYGi-Ow6Af9ZyAB8KzzTkDI1nUg6AQ&usqp=CAU" />
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8r91tqpE7tFkUIeovPUY8z5a90Dw32LRLZWOF_kn96gBVjzryhIm5EkIbQz5V3wLSDBA&usqp=CAU" />
-      </div>
-      <p>
-      Rescuing animals and offering them a home holds immense importance as it not only saves them from neglect or abandonment but also provides a chance for a fulfilling life.
-      Meeting their specific needs is crucial for their well-being, encompassing physical health, emotional support, and a sense of security. Additionally, helping individuals 
-      find a suitable pet companion ensures a harmonious match, fostering lasting connections that benefit both the adopter and the animal. It's a shared responsibility to create 
-      thriving environments for these creatures. A heartfelt thank you goes to those who support these efforts, as their contributions play a vital role in making a positive impact 
-      on the lives of rescued animals and the communities they become a part of.
-      </p>
-  </div>
-  </div>
-      <h1> About Pawsome Prints</h1>
-      <div className="Pawsome">
+      <Nav />
+      <div className="About">
+        <div className="Pawsome">
+          <h1> About Pawsome Prints!!!</h1>
+        </div>
         <div className="firstpics">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRn81BHEGTzMbsr9XNr43MyFpfRzeyh9BY9CQ&usqp=CAU" />
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqKAbyX59TuX8k1KNeC66K0NWZL3PGss8fig&usqp=CAU" />
-            <img src="https://cdn.shopify.com/s/files/1/1891/0077/files/Chi-eating-dubia-roach-1_480x480.png?v=1666631796" />
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRn81BHEGTzMbsr9XNr43MyFpfRzeyh9BY9CQ&usqp=CAU" />
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqKAbyX59TuX8k1KNeC66K0NWZL3PGss8fig&usqp=CAU" />
+        </div>
+        <div className="secondpics">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbqrw3qH4cQdfAE233ZKct5OHCgfATey9bLw&usqp=CAU" />
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0EvPgBYGi-Ow6Af9ZyAB8KzzTkDI1nUg6AQ&usqp=CAU" />
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8r91tqpE7tFkUIeovPUY8z5a90Dw32LRLZWOF_kn96gBVjzryhIm5EkIbQz5V3wLSDBA&usqp=CAU" />
         </div>
         <p>
           Rescuing animals and offering them a home holds immense importance as it not only saves them from neglect or abandonment but also provides a chance for a fulfilling life.
-          Meeting their specific needs is crucial for their well-being, encompassing physical health, emotional support, and a sense of security. Additionally, helping individuals 
-          find a suitable pet companion ensures a harmonious match, fostering lasting connections that benefit both the adopter and the animal. It's a shared responsibility to create 
-          thriving environments for these creatures. A heartfelt thank you goes to those who support these efforts, as their contributions play a vital role in making a positive impact 
+          Meeting their specific needs is crucial for their well-being, encompassing physical health, emotional support, and a sense of security. Additionally, helping individuals
+          find a suitable pet companion ensures a harmonious match, fostering lasting connections that benefit both the adopter and the animal. It's a shared responsibility to create
+          thriving environments for these creatures. A heartfelt thank you goes to those who support these efforts, as their contributions play a vital role in making a positive impact
+          on the lives of rescued animals and the communities they become a part of.
+        </p>
+      </div>
+      <h1> About Pawsome Prints</h1>
+      <div className="Pawsome">
+        <div className="firstpics">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRn81BHEGTzMbsr9XNr43MyFpfRzeyh9BY9CQ&usqp=CAU" />
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqKAbyX59TuX8k1KNeC66K0NWZL3PGss8fig&usqp=CAU" />
+          <img src="https://cdn.shopify.com/s/files/1/1891/0077/files/Chi-eating-dubia-roach-1_480x480.png?v=1666631796" />
+        </div>
+        <p>
+          Rescuing animals and offering them a home holds immense importance as it not only saves them from neglect or abandonment but also provides a chance for a fulfilling life.
+          Meeting their specific needs is crucial for their well-being, encompassing physical health, emotional support, and a sense of security. Additionally, helping individuals
+          find a suitable pet companion ensures a harmonious match, fostering lasting connections that benefit both the adopter and the animal. It's a shared responsibility to create
+          thriving environments for these creatures. A heartfelt thank you goes to those who support these efforts, as their contributions play a vital role in making a positive impact
           on the lives of rescued animals and the communities they become a part of.
         </p>
         <div className="secondpics">
