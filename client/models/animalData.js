f3a961170e844d6a7a8dd59d7831c2e8411d666d
Tesean Ferguson <tferguson1028@outlook.com>
2024-01-05 11:06:30 -0500

    Updated classes for animal data

--- client/models/animalData.js
+++ client/models/animalData.js
@@ -3,24 +3,37 @@ class Animal
   name;
   picture;
   animalInfo;
-  constructor(name, picture, animalInfo)
+  owner;
+  constructor(name, picture, animalInfo, owner = null)
   {
     this.name = typeof name === "string" ? name : "No Name Given";
     this.picture = typeof picture === "string" ? picture : "https://example.com";
     this.animalInfo = animalInfo instanceof AnimaInfo ? animalInfo : new AnimalInfo();
+    this.owner = String(owner);
   }
 }
 
 class AnimalInfo
 {
-  constructor()
+  description;
+  personality;
+  health;
+  issues;
+  story;
+  constructor(description, personality, health, issues, story)
   {
-  
+    this.description = String(description);
+    this.personality = String(personality);
+    this.health = String(health);
+    this.issues = String(issues);
+    this.story = String(story);
   }
 }
 
 
 const animalData = [
-
+  
   
 ];
+
+module.exports = animalData;
