c4854bfaaf3b34a6181dc6c42e59dc110a38499d
drebyrd715 <drebyrd715@gmail.com>
2024-01-05 11:39:15 -0500

    introduction

--- client/src/components/introduction/Introduction.jsx
+++ client/src/components/introduction/Introduction.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 function Introduction() {
-  return <div>Introductions</div>;
+  return <div className="Section1">
+    <div>
+      <img sr ="https://m.media-amazon.com/images/I/81STn23zZnL.png"/>
+    </div>
+    <div>
+    <p>Pawsome Print is a compassionate online platform connecting individuals with rescue animals 
+    seeking loving homes. Users can browse a variety of adorable pets and make donations, 
+    supporting the mission of animal rescue organizations. It's a heartwarming way to find your new friend 
+    while contributing to a noble cause.</p>
+    </div>
+
+    <div className="Section2">
+    <p>
+    In the quiet corners of our hearts, we harbored a profound belief that every rescued animal 
+    carried a unique tale of resilience and hope. Driven by an unyielding passion for their well-being, 
+    we embarked on a journey to create a company that would connect these resilient souls with individuals seeking 
+    not just companionship, but stories of redemption and unconditional love. Our mission was born from a desire to transform lives,
+    both human and animal, forging a bond that transcends the ordinary and gives purpose to our shared journey.
+    </p>
+    </div>
+
+    </div>;
 }
 
 export default Introduction;
