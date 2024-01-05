8f01ece2d71ab3f833c471be19de267795d096d0
drebyrd715 <drebyrd715@gmail.com>
2024-01-05 14:04:36 -0500

    server

--- client/src/components/animalsComponent/AnimalsComponent.jsx
+++ client/src/components/animalsComponent/AnimalsComponent.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function AnimalsComponent() {
-  return <div>AnimalsComponent</div>;
+  return <div>
+    <a href="/animals">
+  <h2>View Animals</h2>
+   </a>
+  </div>;
 }
 
 export default AnimalsComponent;
